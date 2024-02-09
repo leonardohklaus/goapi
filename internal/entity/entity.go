22c5373ea131c946c8624393c7a09a827ec33390
Leonardo H. Klaus <leonardohklaus@icloud.com>
2024-02-09 09:56:25 -0300

    Remove RabbitMQ connection

--- internal/entity/entity.go
+++ internal/entity/entity.go
@@ -3,8 +3,8 @@ package entity
 import "github.com/google/uuid"
 
 type Category struct {
-	ID   string
-	Name string
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 func NewCategory(name string) *Category {
@@ -15,12 +15,12 @@ func NewCategory(name string) *Category {
 }
 
 type Product struct {
-	ID          string
-	Name        string
-	Description string
-	CategoryID  string
-	ImageURL    string
-	Price       float64
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	CategoryID  string `json:"category_id"`
+	ImageURL    string `json:"image_url"`
+	Price       float64 `json:"price"`
 }
 
 func NewProduct(name, description, categoryID, imageURL string, price float64) *Product {
--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -3,7 +3,6 @@ package service
 import (
 	"github.com/leonardohklaus/goapi/internal/database"
 	"github.com/leonardohklaus/goapi/internal/entity"
-	"github.com/leonardohklaus/goapi/internal/rabbitmq"
 )
 
 type ProductService struct {
@@ -44,11 +43,6 @@ func (ps *ProductService) CreateProduct(name, description, category_id, image_ur
 	if err != nil {
 		return nil, err
 	}
-
-	err = rabbitmq.PublishRabbitMQMessage(product)
-		if err != nil {
-			panic(err.Error())
-		}
 
 	return product, nil
 }
